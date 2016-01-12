a0b1f9811c02f1638e0ba4dfdc81bd4624b61fe3
Valentin Brclz <ValentinBrclz@users.noreply.github.com>
2016-01-12 20:51:59 +0100

    Creation of the database files

---
+++ src/main/java/model/DatabaseInterface.java
@@ -0,0 +1,31 @@
+/*
+ * Java RESTful Transaction
+ *
+ * Wikipedia FR Bot that signs when users forget to do so
+ * Copyright (C) 2016 Valentin Berclaz
+ * <http://www.valentinberclaz.com/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package model;
+
+/**
+ * Interface that defines the function to implement by any database
+ *
+ * @author Valentin Berclaz
+ */
+public interface DatabaseInterface {
+
+}
