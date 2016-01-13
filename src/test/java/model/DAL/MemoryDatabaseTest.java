c43c13cf43aab4104fae8ddf5dd3ba4169c64634
Valentin Brclz <ValentinBrclz@users.noreply.github.com>
2016-01-13 15:18:39 +0100

    Creation of the tests

---
+++ src/test/java/model/DAL/MemoryDatabaseTest.java
@@ -0,0 +1,58 @@
+/*
+ * Java RESTful Transaction
+ *
+ * Wikipedia FR Bot that signs when users forget to do so
+ * Copyright (C) 2016 Valentin Berclaz
+ * <http://www.valentinberclaz.com/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package model.DAL;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test of the class "MemoryDatabase"
+ *
+ * @author Valentin Berclaz
+ */
+public class MemoryDatabaseTest {
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@Test
+	public void testGetTransaction() throws Exception {
+
+	}
+
+	@Test
+	public void testGetTransactionsIdsByType() throws Exception {
+
+	}
+
+	@Test
+	public void testGetTransactionChildren() throws Exception {
+
+	}
+
+	@Test
+	public void testAddTransaction() throws Exception {
+
+	}
+}
